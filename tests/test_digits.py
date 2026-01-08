d9c56160dd6e7f0f5912ce726f892f1ecfe74554
Lord.Aslan <aslani80.m@gmail.com>
2026-01-08 11:12:43 +0330

    add new tests for currency tool

--- tests/test_digits.py
+++ tests/test_digits.py
@@ -23,3 +23,36 @@ class Testdigits(TestCase):
         self.assertEqual(digits("مامور ۰۰۷", "fe"), "مامور 007")
         self.assertEqual(digits("dr۴۶۰nized", "fe"), "dr460nized")
         self.assertEqual(digits("سل۱م د۹ست من", "fe"), "سل1م د9ست من")
+
+    def test_currency_words_less_than_10(self):
+        self.assertEqual(digits("7", "cw"), "هفت")
+
+    def test_currency_words_less_than_100(self):
+        self.assertEqual(digits("70", "cw"), "هفتاد")
+        self.assertEqual(digits("99", "cw"), "نود و نه")
+        self.assertEqual(digits("15", "cw"), "پانزده")
+
+    def test_currency_words_less_than_1000(self):
+        self.assertEqual(digits("005", "cw"), "پنج")
+        self.assertEqual(digits("300", "cw"), "سیصد")
+        self.assertEqual(digits("370", "cw"), "سیصد و هفتاد")
+        self.assertEqual(digits("372", "cw"), "سیصد و هفتاد و دو")
+        self.assertEqual(digits("312", "cw"), "سیصد و دوازده")
+        self.assertEqual(digits("905", "cw"), "نهصد و پنج")
+
+    def test_currency_words_less_than_10000(self):
+        self.assertEqual(digits("1000", "cw"), "یک هزار")
+        self.assertEqual(digits("1300", "cw"), "یک هزار و سیصد")
+        self.assertEqual(digits("5370", "cw"), "پنج هزار و سیصد و هفتاد")
+        self.assertEqual(digits("2372", "cw"), "دو هزار و سیصد و هفتاد و دو")
+        self.assertEqual(digits("7312", "cw"), "هفت هزار و سیصد و دوازده")
+        self.assertEqual(digits("8003", "cw"), "هشت هزار و سه")
+
+    def test_currency_words_less_than_100000(self):
+        self.assertEqual(digits("13295", "cw"), "سیزده هزار و دویست و نود و پنج")
+        self.assertEqual(digits("549817", "cw"), "پانصد و چهل و نه هزار و هشتصد و هفده")
+        self.assertEqual(digits("900001", "cw"), "نهصد هزار و یک")
+
+    def test_currency_words_less_than_1000000(self):
+        self.assertEqual(digits("1000000", "cw"), "یک میلیون")
+        self.assertEqual(digits("7002005", "cw"), "هفت میلیون و دو هزار و پنج")
